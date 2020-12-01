e6af126602cf7d9d5daf12ee47e06f4df1040e95
Vivid Green <zacharycreek@gmail.com>
2020-11-30 17:40:16 -0700

    Added syncDB function for offline functionality. Added manifest and sw files for PWA.

---
+++ public/sw.js
@@ -0,0 +1,108 @@
+console.log("hit sw.js");
+
+const fileCacheName = "file-v3";
+
+const filesToCache = [
+    "/",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "manifest.webmanifest"
+];
+
+const dataCacheName = "data-v2";
+
+self.addEventListener("install", (event) => {
+    console.log("hit install");
+
+    event.waitUntil(
+        caches
+        .open(fileCacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache);
+        })
+        .catch(error => console.log("error caching files on install: ", error))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("active", (event) => {
+    console.log("hit activate");
+
+    event.waitUntil(
+        caches
+        .keys()
+        .then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    console.log(key);
+                    if(key !== fileCacheName && key !== dataCacheName) {
+                        console.log("deleting cache: ", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+        .catch(error => console.log("activation error: ", error))
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    console.log(event);
+
+    if(event.request.url.includes("/api")) {
+        return event.respondWith(
+            caches
+            .open(dataCacheName)
+            .then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if(response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                .catch(error => {
+                   return cache.match(event.request); 
+                });
+            })
+            .catch(error => console.log("error fetching api: ", error))
+        );
+    }
+
+    event.respondWith(
+        caches
+        .match(event.request)
+        .then(response => {
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request)
+            .then((response) => {
+                if(!response || !response.basic || !response.status !== 200) {
+                    console.log("fetch response: ", response);
+                    return response;
+                }
+
+                const responseToCache = response.clone();
+
+                caches
+                .open(cacheName)
+                .then(cache => {
+                    cache.put(event.request, responseToCache);
+                })
+                .catch(error => console.log(error));
+
+                return response;
+            });
+        })
+        .catch(error => console.log(error))
+    );
+});
